4960667b30a22b81ce62cd867608382e6ecd5c69
Sam Black <sam.black@lubbockisd.org>
2023-08-30 09:24:13 -0500

    continued lesson including escape sequences

--- src/FirstJavaProgram.java
+++ src/FirstJavaProgram.java
@@ -1,5 +1,18 @@
-public class Main {
+public class FirstJavaProgram {     // This is a comment to describe what this Java class does
+
+    // This is the main method demonstrating how to output to the console.
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Howdy y'all!!");
+        System.out.print("How's it going?");
+        System.out.println("I'm good.");
+
+        //printf -- formatted print
+
+        //-------------
+        // Escape sequences -
+        System.out.println("Hello\n World");  // newline character
+        System.out.println("Howdy\t y'all!\b");  // tab character and backspace character
+
+
     }
 }
